9ce49cf2990d376cafba7ba4178f321e7d81eab6
atournikate <kate.ebensteiner@gmail.com>
2021-08-25 14:52:23 +0200

    hello me, it is me

---
+++ www.opportunity-zueri.ch/projects/js_nutshell/home.php
@@ -0,0 +1,85 @@
+<?php 
+  require_once $_SERVER['HOME'].'/projects/js_nutshell/config.php';
+  require_once 'header.inc.php';
+
+?>
+    
+
+    <div id="slider1" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#slider1" data-slide-to="0" class="active"></li>
+        <li data-target="#slider1" data-slide-to="1"></li>
+        <li data-target="#slider1" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">b5-carousel-item</div>
+      <a
+        class="carousel-control-prev"
+        href="#slider1"
+        role="button"
+        data-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#slider1"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <div class="teaser">
+      <p>
+        This is text here. Prow scuttle parrel provost Sail ho shrouds spirits
+        boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's
+        nest nipperkin grog yardarm hempen halter furl. Swab barque interloper
+        chantey doubloon starboard grog black jack gangway rutters.
+      </p>
+    </div>
+
+    <article>
+      <h2>This is a Header 2</h2>
+      <p>
+        Deadlights jack lad schooner scallywag dance the hempen jig carouser
+        broadside cable strike colors. Bring a spring upon her cable holystone
+        blow the man down spanker Shiver me timbers to go on account lookout
+        wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot
+        yardarm spyglass sheet transom heave to.
+      </p>
+    </article>
+    <div class="codeArea">
+      <h3>Header 3 for some reasons</h3>
+      <p>
+        Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway.
+        Case shot Shiver me timbers gangplank crack Jennys tea cup ballast
+        Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas
+        boatswain schooner gaff booty Jack Tar transom spirits.
+      </p>
+      <div class="container">
+        <pre>
+                code here
+            </pre
+        >
+        <button class="btn btn-run"></button>
+      </div>
+    </div>
+    <div class="toc">
+      <ul>
+        <li><a href="#">link 1</a></li>
+        <li><a href="#">link 2</a></li>
+        <li><a href="#">link 3</a></li>
+        <li><a href="#">link 4</a></li>
+        <li><a href="#">link 5</a></li>
+        <li><a href="#">link 6</a></li>
+      </ul>
+    </div>
+    <footer>
+      <p>Created by Opportunity, 2021</p>
+    </footer>
+  </body>
+  <script src="../js_nutshell/js/bootstrap.bundle.js"></script>
+</html>
---
+++ www.opportunity-zueri.ch/projects/js_nutshell/inc/header.inc.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home Template</title>
+    <link rel="stylesheet" href="../js_nutshell/css/bootstrap.css" />
+    <link rel="stylesheet" href="../js_nutshell/css/mainStyle.css  " />
+  </head>
+  <body>
+    <header>
+      <h1>Header 1 Here</h1>
+
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">primary</a>
+          <img src="" alt="" class="siteLogo" />
+
+          <button
+            class="btn navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarID"
+            aria-controls="navbarID"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarID">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <button class="btn">Click</button>
+    </header>

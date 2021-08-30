ed2fc678c466040aea7a44f44430fd8f4b872c9f
atournikate <kate.ebensteiner@gmail.com>
2021-08-30 16:41:11 +0200

    finalize template kme

--- www.opportunity-zueri.ch/projects/js_nutshell/inc/footer.inc.php
+++ www.opportunity-zueri.ch/projects/js_nutshell/inc/footer.inc.php
@@ -1,9 +1,9 @@
 <!--BUTTON!!! -->
-<button id="myHome" title="Back to Home" href="/projects/js_nutshell/home.php">Home</button>
 <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+<button id="myHome" title="Back to Home" href="/projects/js_nutshell/home.php">Home</button>
 
 <footer class="d-flex justify-content-center align-items-end text-center">
-      <p class="mt-3">Created by Opportunity, 2021</p>
+      <p class="mt-3">Created by Opportunity Zürich, 2021</p>
     </footer>
   </body>
    <script src="../js_nutshell/js/bootstrap.bundle.js"></script>
